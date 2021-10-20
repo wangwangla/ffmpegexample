da7f29600e0a4a5cc0746518a245cf53712f05a4
28188 <2818815189@qq.com>
2021-10-20 08:23:17 +0800

    音效

--- app/src/main/cpp/AudioChannel.cpp
+++ app/src/main/cpp/AudioChannel.cpp
@@ -10,4 +10,72 @@
  */
 AudioChannel::AudioChannel(int id):BaseChannel(id) {
 
+
+    SLresult result;
+    result = slCreateEngine(&engineObject,0,NULL,0,NULL,NULL);
+    if (result!=SL_RESULT_SUCCESS){
+        return;
+    }
+    result = (*engineObject)->Realize(engineObject,SL_BOOLEAN_FALSE);
+    if (result != SL_RESULT_SUCCESS){
+        return;
+    }
+
+    result = (*engineObject)->GetInterface(
+            engineObject,
+            SL_IID_ENGINE,
+            &engineInterface
+            );
+
+
+    if(result != SL_RESULT_SUCCESS){
+        return;
+    }
+
+    //2
+    result = (*engineInterface)->CreateOutputMix(engineInterface,
+                                           &outputMixObject,
+                                           0,
+                                           0,0);
+    if (SL_RESULT_SUCCESS != result){
+        return;
+    }
+
+    result = (*outputMixObject)->Realize(outputMixObject,SL_BOOLEAN_FALSE);
+    if (result!=SL_RESULT_SUCCESS){
+        return;
+    }
+
+    //3创建播放器
+    SLDataLocator_AndroidBufferQueue android_queue = {
+        SL_DATALOCATOR_ANDROIDSIMPLEBUFFERQUEUE,2
+    };
+
+    SLDataFormat_PCM pcm = {
+            SL_DATAFORMAT_PCM,2,
+            SL_SAMPLINGRATE_44_1,
+            SL_PCMSAMPLEFORMAT_FIXED_16,
+            SL_PCMSAMPLEFORMAT_FIXED_16,
+            SL_SPEAKER_FRONT_LEFT|SL_SPEAKER_FRONT_RIGHT,
+            SL_BYTEORDER_LITTLEENDIAN
+    };
+    SLDataSource slDataSource = {
+            &android_queue,&pcm
+    };
+
+    SLDataLocator_OutputMix outputMix = {
+        SL_DATALOCATOR_OUTPUTMIX,
+        outputMixObject
+    };
+
+    SLDataSink audioSnk = {
+            &outputMix,NULL
+    };
+    const SLInterfaceID ids[1] = {SL_IID_BUFFERQUEUE};
+    const SLboolean req[1] = {SL_BOOLEAN_TRUE};
+
+    (*engineInterface)->CreateAudioPlayer(
+            engineInterface,
+
+            );
 }
--- app/src/main/cpp/AudioChannel.h
+++ app/src/main/cpp/AudioChannel.h
@@ -7,13 +7,30 @@
 
 
 #include "BaseChannel.h"
+#include <SLES/OpenSLES.h>
+#include <SLES/OpenSLES_Android.h>
 
 class AudioChannel: public BaseChannel {
 public:
-    AudioChannel(int id);
-    virtual ~AudioChannel();
+    AudioChannel(int id,AVCodecContext *avCodecContext);
+    ~AudioChannel();
+    void play();
+    void decode();
+    void _play();
+    void getPcm();
     int audioChannel;
-
+private:
+    pthread_t pid_audio_decode;
+    pthread_t pid_audio_play;
+    //引擎和接口
+    SLObjectItf engineObject = 0;
+    SLEngineItf engineInterface = 0;
+    //混音
+    SLObjectItf outputMixObject = 0;
+    //播放器
+    SLObjectItf audioPlayerObject;
+    SLAndroidSimpleBufferQueueItf androidSimpleBufferQueueItf;
+    SLPlayItf audioPlayInterface;
 };
 
 
